f2dbac8ca726e0d6e0583dfb2ceac8f63e324041
HASSAN ハッサン <console041@gmail.com>
2018-12-31 19:09:31 +0900

    Create devops.ps1

---
+++ devops.ps1
@@ -0,0 +1,14 @@
+cinst docker-for-windows
+
+cinst virtualbox
+cinst virtualbox.extensionpack
+cinst vagrant
+
+# SSH is available from cmder
+
+#Azure Cli
+cinst azure-cli
+
+#AWS Cli
+cinst awscli
+cinst awstools.powershell
