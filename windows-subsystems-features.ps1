0bd14d920fa4d6efa3ff8ca142c0c79d53756acd
HASSAN ハッサン <console041@gmail.com>
2018-12-31 19:16:39 +0900

    Update windows-subsystems-features.ps1

--- windows-subsystems-features.ps1
+++ windows-subsystems-features.ps1
@@ -1,7 +1,4 @@
 Enable-RemoteDesktop
 cinst Microsoft-Windows-Subsystem-Linux -source windowsfeatures
 cinst Microsoft-Hyper-V-All -source windowsFeatures
-cinst docker-for-windows
-cinst kubernetes-cli
 cinst Containers -source windowsFeatures
-cinst azure-cli
