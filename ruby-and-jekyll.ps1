c048b8d1d3afefdb1578855e94fede292c257ca2
HASSAN ハッサン <console041@gmail.com>
2018-12-31 21:07:05 +0900

    Update ruby-and-jekyll.ps1

--- ruby-and-jekyll.ps1
+++ ruby-and-jekyll.ps1
@@ -1,8 +1,12 @@
-cinst msys2
-refreshenv
-cinst ruby
-refreshenv
-cinst ruby.devkit
-refreshenv
+#method-1: (in my case, error while installing jekyll)
+#cinst msys2
+#refreshenv
+#cinst ruby
+#refreshenv
+#cinst ruby.devkit
+#refreshenv
+#gem install bundler jekyll
 
-gem install bundler
+# method-2: download Ruby+Devkit installer and install it first (in case error while installing jekyll)
+# https://rubyinstaller.org/downloads/
+gem install bundler jekyll
